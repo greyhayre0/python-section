01294f3ea18e1a9d26c4eabf6053d92cc92fdb74
Grey-Hayred <greyhayre1@gmail.com>
2026-01-13 11:28:41 +0300

    descriptors prerol

--- src/descriptors/models.py
+++ src/descriptors/models.py
@@ -9,4 +9,33 @@ class Model:
 
 
 class Field:
-    pass
+    '''Дескриптор данных'''
+    def __init__(self, path: str):
+        self.path = path
+
+    def __get__(self, instance: 'Model', owner) -> Any: # Либо через класс получаем дескриптор либо через экземпляр то вызов метода чтение
+        if instance is None:
+            return self
+        return self._get_value(instance.payload)
+
+    def __set__(self, instance: 'Model', value: Any) -> None: # Для присвоения атрибута используем кастом функцию запись
+        self._set_value(instance.payload)
+
+    def _get_value(self, payload: JSON, value: Any) -> Any: # Извлекатель
+        parts = self.path.split('.')
+        current = payload
+        for part in parts:
+            if isinstance(current, dict) and part in current:
+                current = current[part]
+            else:
+                return None
+            return current
+
+    def _set_value(self, payload: JSON, value: Any) -> None: # Ну по сути записывает данные
+        parts = self.path.split('.')
+        current = payload
+        for part in parts[:-1]:
+            if parts not in current or not isinstance(current[part], dict):
+                current[part] = {}
+            current = current[part]
+        current[parts[-1]] = value
