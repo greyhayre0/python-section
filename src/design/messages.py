c5d70986da44426a1987fcb8409500557178923d
Grey-Hayred <greyhayre1@gmail.com>
2026-01-14 17:27:32 +0300

    Parse preroll

--- src/design/messages.py
+++ src/design/messages.py
@@ -1,5 +1,7 @@
+from abc import ABC, abstractmethod
 import enum
 from dataclasses import dataclass
+from typing import Dict, Optional
 
 
 class MessageType(enum.Enum):
@@ -17,3 +19,55 @@ class JsonMessage:
 @dataclass
 class ParsedMessage:
     """There is no need to describe anything here."""
+
+
+class MessageParsed(ABC):
+    '''Абстракция для всех'''
+    @abstractmethod
+    def parse(self, payload: str) -> ParsedMessage:
+        pass
+
+
+class ParserFactory:
+    '''Фабрика'''
+    def __init__(self):
+        self._parsers: Dict[MessageType, MessageParsed] = {}
+
+    def register(self, m_type: MessageType, parser: MessageParsed) -> None:
+        self._parsers[m_type] = parser
+
+    def create(self, m_type: MessageType) -> Optional[MessageParsed]:
+        return self._parsers.get(m_type)
+
+    def parse(self, json_m: JsonMessage):
+        parser = self.create(json_m.message_type)
+        if not parser:
+            raise ValueError
+        return parser.parse(json_m.payload)
+
+
+class TelegramParser(MessageParsed):
+    def parse(self, payload: str) -> ParsedMessage:
+        print('Telegramm')
+        return ParsedMessage()
+
+
+class MattermostParser(MessageParsed):
+    def parse(self, payload: str) -> ParsedMessage:
+        print('Mattermost')
+        return ParsedMessage()
+
+
+class SlackParser(MessageParsed):
+    def parse(self, payload: str) -> ParsedMessage:
+        print('Slack')
+        return ParsedMessage()
+
+
+factory = ParserFactory()
+factory.register(MessageType.TELEGRAM, TelegramParser())
+factory.register(MessageType.MATTERMOST, MattermostParser())
+factory.register(MessageType.SLACK, SlackParser())
+
+message = JsonMessage(MessageType.TELEGRAM, '{"Br":"Go SIng"}')
+result = factory.parse(message)
